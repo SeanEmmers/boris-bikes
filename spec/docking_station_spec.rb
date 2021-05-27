7e6ae3e667305a840cc0f54c4873855703a194a3
Sean Phillips <seanphillips@Seans-Air.broadband>
2021-05-27 16:10:34 +0100

    Adding an @bikes array

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,6 @@
 require 'docking_station'
 
-#Does DockingStation respond to release_bike
+# Does DockingStation respond to release_bike
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike)}
 
@@ -8,19 +8,19 @@ describe DockingStation do
   it { is_expected.to respond_to(:dock).with(1).argument }
 
 # Does DockingStation respond to the bike method
-  it { is_expected.to respond_to(:bike)}
+  it { is_expected.to respond_to(:bikes)}
 
 # Does the method dock(bike) return an instance of the Bike class?
   it 'docks something' do
     bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
+    expect(subject.dock(bike)).to eq [bike]
   end
 
 # Does the method .bike return bike on the dock(bike) method?
   it 'returns docked bikes' do
     bike = Bike.new
     subject.dock(bike)
-    expect(subject.bike).to eq bike
+    expect(subject.bikes).to eq [bike]
   end
 
 # Does the .release_bike method return bike
@@ -37,9 +37,10 @@ describe '#release_bike' do
   end
 end
 
+# Dock method will return an error when it already contains @bike
 describe '#dock' do
   it 'raises an error when full' do
-    subject.dock(Bike.new)
+    20.times { subject.dock Bike.new } 
     expect { subject.dock Bike.new }.to raise_error 'Docking station is full'
   end
 end
